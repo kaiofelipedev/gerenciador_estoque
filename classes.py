f6e356309650d38dff79d7aab0c4356e3aeb340e
Kaio Felipe <kaio.felipe.santos@hotmail.com>
2024-10-27 13:14:16 -0300

    configurações da janela

--- classes.py
+++ classes.py
@@ -3,5 +3,11 @@ from tkinter import *
 class App():
     def __init__(self):
         self.window = Tk()
-        self.window.title("GereFácil App")
+        self.settings()
         mainloop()
+
+    def settings(self):
+        self.window.title("GereFácil App")
+        self.window.geometry("480x400")
+        self.window.configure(background="#262626")
+        self.window.minsize(width= 480, height= 400)

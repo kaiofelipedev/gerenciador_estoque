cab080f1b7ce7cb68770538e4c8bc974e8755c54
Kaio Felipe <kaio.felipe.santos@hotmail.com>
2024-10-29 02:14:11 -0300

    mudança nos requisitos

--- classes.py
+++ classes.py
@@ -1,21 +1,28 @@
 from tkinter import *
 
-class App():
+class Buttons():
+    def botao1(self):
+        self.botao_adm = Button(self.window, text= "administrador".upper())
+        self.botao_adm.place(relx= 0.25, rely= 0.3, relwidth= 0.5, relheight= 0.15)
+
+    def botao2(self):
+        self.botao_user = Button(self.window, text= "usuário".upper())
+        self.botao_user.place(relx= 0.25, rely= 0.5, relwidth= 0.5, relheight= 0.15)
+
+class App(Buttons):
     def __init__(self):
         self.window = Tk()
         self.settings()
-        self.botoes()
+        self.botao1()
+        self.botao2()
         mainloop()
-
+# Configurações da janela
     def settings(self):
         self.window.title("GereFácil App")
         self.window.geometry("480x400")
-        self.window.configure(background="#262626")
+        self.window.configure(background="#F2522E")
         self.window.minsize(width= 480, height= 400)
+# Tela inicial
 
-    def botoes(self):
-        self.botao_adm = Button(self.window, text="administrador".upper(), background="#A6A6A6")
-        self.botao_adm.place(relx= 0.2, rely= 0.25, relwidth= 0.6, relheight= 0.2)
-
-        self.botao_usuario = Button(self.window, text="usuário".upper(), background="#A6A6A6")
-        self.botao_usuario.place(relx= 0.2, rely= 0.55, relwidth= 0.6, relheight= 0.2)
+    def frame_baixo(self):
+        pass

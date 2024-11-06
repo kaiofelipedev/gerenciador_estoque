76d68041895f11173e2c51a8833ac7ede6cefee4
Kaio Felipe <kaio.felipe.santos@hotmail.com>
2024-11-06 19:07:38 -0300

    criação da tela de Adm(falta Treeview)

--- index.py
+++ index.py
@@ -74,13 +74,52 @@ class TelaLogin:
                      font=("Verdana", 10, "bold"), background="#F2522E", activebackground="#F27B35",activeforeground="white",
                      command=self.voltar_tela_inicial)
         self.bt_voltar.place(relx=.73, rely=0.01, relwidth=.25, relheight=.15)
+        # Botão 'Entrar'
+        self.bt_entrar = Button(self.frm_login, text="Entrar", relief=RAISED, bd=6,
+                     font=("Verdana", 10, "bold"), background="#F2522E", activebackground="#F27B35",activeforeground="white")
+        self.bt_entrar.place(relx=.73, rely=.83, relwidth=.25, relheight=.15)
 
     def voltar_tela_inicial(self):
         self.frm_login.place_forget()
         TelaInicial()
 
+class TelaAdm:
+    def __init__(self):
+        self.frm_adm = Frame(window, background="#D9AD29", highlightthickness=1,
+                  highlightcolor="#592202", border=5, relief=RAISED)
+        self.frm_adm.place(relx=0, rely=0, relwidth=1.0, relheight=1.0)
+        # Label 'Adm'
+        self.lb_adm = Label(self.frm_adm,  text="Adm", border=4, relief=SUNKEN,
+                        font=("Verdana", 14, "bold"), background="#592202", highlightthickness=4,
+                        fg="white")
+        self.lb_adm.place(relwidth=.2, relheight=.12)
+        # Botão 'Sair'
+        self.bt_sair = Button(self.frm_adm, text="Sair", fg="white", border=4,
+                            font=("Verdana", 10, "bold"),highlightthickness=4,relief=RAISED, background="#F2522E",
+                            highlightbackground="#592202", command=self.inicio)
+        self.bt_sair.place(relx=.9, relwidth=.1, relheight=.08)
+        # Botão 'Novo Produto'
+        self.novo_produto = Button(self.frm_adm,  text="Novo Produto", border=8,
+                        font=("Verdana", 9, "bold"), relief=SUNKEN, fg="white",
+                        background="#592202")
+        self.novo_produto.place(relx=.01, rely=.87, relwidth=.2, relheight=.12)
+        # Botão 'Novo Usuário'
+        self.novo_user = Button(self.frm_adm,  text="Novo Usuário", border=8,
+                        font=("Verdana", 9, "bold"), relief=SUNKEN, fg="white",
+                        background="#592202")
+        self.novo_user.place(relx=.4, rely=.87, relwidth=.2, relheight=.12)
+        # Botão 'Gráfico'
+        self.bt_grafico = Button(self.frm_adm,  text="Gráfico", border=8,
+                        font=("Verdana", 9, "bold"), relief=SUNKEN, fg="white",
+                        background="#592202")
+        self.bt_grafico.place(relx=.79, rely=.87, relwidth=.2, relheight=.12)
+
+    def inicio(self):
+        self.frm_adm.place_forget()
+        TelaInicial()
 # Variável recebendo o Tk (janela) 
 window = Tk()
 Janela(window)
-TelaInicial()
+TelaAdm()
+#TelaInicial()
 window.mainloop()# executando o loop para mostrar a janela
